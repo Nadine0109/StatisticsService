bb61223397d7f3982cb7b3d1d8f033c363909ee7
Nadine0109 <nadine0109@ya.ru>
2021-03-03 19:20:15 +0500

    Test added for 100 percent coverage

--- src/test/java/ru/netology/statisticsservice/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statisticsservice/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findCurrentMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 12, 8, 6, 11, 11, 12};
+
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
